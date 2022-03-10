50c35491f3d3585975431bf41feab911d0976aa8
Jandres1420 <juan.pico-m@mail.escuelaing.edu.co>
2022-03-09 22:33:54 -0500

    First commit

---
+++ encriptacion_de_logs/src/main/java/edu/eci/spti/logs/model/logs.java
@@ -0,0 +1,18 @@
+package edu.eci.spti.logs.model;
+
+public class logs {
+    private String log;
+
+    public logs(String log) {
+        this.log = log;
+    }
+
+    public String getLog(){
+        return log;
+    }
+
+    public void setLog(String log) {
+        this.log = log;
+    }
+
+}
